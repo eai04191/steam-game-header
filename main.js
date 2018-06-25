2a7d5f1d3a25520ff6b718188418a865dc4e6419
eai04191 <eai@mizle.net>
2018-06-25 19:36:42 +0900

    Add error handling, auto reflash, Processing when there is no game.

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
-const key = getParm("key");
-const steamid = getParm("steamid");
-const URL = `https://script.google.com/macros/s/AKfycbwTz8vBIAkYec3rgfxE9KYBnkHMpwo5S0zbPwQzmqfwYxdBPRA/exec?key=${key}&steamid=${steamid}`;
+const key = getParam("key");
+const steamid = getParam("steamid");
+const refresh = getParam("refresh");
+const scriptURL = "https://script.google.com/macros/s/AKfycbwTz8vBIAkYec3rgfxE9KYBnkHMpwo5S0zbPwQzmqfwYxdBPRA/exec";
+const URL = `${scriptURL}?key=${key}&steamid=${steamid}`;
 
-function getParm(key) {
+function getParam(key) {
     let regExpString = key + "=(.*?)(&|$)";
     let regExp = new RegExp(regExpString);
-    return decodeURIComponent(window.location.search.match(regExp)[1]);
+    if (window.location.search.match(regExp)) {
+        return window.location.search.match(regExp)[1];
+    } else {
+        return null;
+    }
 }
 
-fetch(URL).then(function (response) {
-    response.text().then(function (json) {
+function msg(message) {
+    document.getElementById("msg").innerHTML = message;
+}
+
+if (key === null || steamid === null) {
+    msg("<kbd>key</kbd> or <kbd>steamid</kbd> is not set. :(");
+} else {
+    fetch(URL).then(function (response) {
+        if (response.ok) {
+            return response.text();
+        }
+        throw new Error("Network response was not ok.");
+    }).then(function (json) {
         let url = JSON.parse(json)["url"];
-        document.getElementById("img").innerHTML = `<img src="${url}" width="460" height="215">`;
+        if (url != "#") {
+            document.getElementById("img").innerHTML = `<img src="${url}" width="460" height="215">`;
+        }
+    }).catch(function (error) {
+        console.log(`error: ${error}`);
+        msg("Data could not be retrieved. Probably the parameter is wrong.");
     });
-});
+};
+
+if (refresh) {
+    let time = refresh * 60;
+    let tag = `<meta http-equiv="refresh" content="${time};URL=${location.href}" />`;
+    document.head.innerHTML = document.head.innerHTML + tag;
+}
